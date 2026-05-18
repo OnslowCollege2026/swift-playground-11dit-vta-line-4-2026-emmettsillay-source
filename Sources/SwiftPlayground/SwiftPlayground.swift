1e02190343e5e799ab6a33aa67d90d76129edc59
emmettsillay-source <emmett.sillay@student.onslow.school.nz>
2026-05-18 21:59:22 +0000

    May 18, 2026, 9:59 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,24 +4,5 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        var isRunning = true
-print("What is your age?")
-
-while isRunning {
-    let input = readLine()
-    
-    if age <= 0 || age >= 122 {
-        print("Age incorrect.")
-    } else if age <= 18 {
-        print("Too young.")
-    } else {
-        print("You may enter")
     }
-} else {
-    print("Please enter a number.")
-}
-
-    }
-    }
-    
 }
