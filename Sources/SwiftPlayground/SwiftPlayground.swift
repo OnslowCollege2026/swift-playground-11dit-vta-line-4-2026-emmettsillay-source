1e33746f6936defb09c1bff0fce346477df1e96c
emmettsillay-source <emmett.sillay@student.onslow.school.nz>
2026-05-18 21:39:51 +0000

    May 18, 2026, 9:39 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,24 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        // Test that it works as it should using the testing table
+// Task - Only to accept people in if they are over the age of 18 
+
+print("What is your age?")
+
+if let input = readLine(), let age = Int(input) {
+    if age <= 0 || age >= 122 {
+        print("Age incorrect.")
+    } else if age <= 18 {
+        print("Too young.")
+    } else {
+        print("You may enter")
     }
+} else {
+    print("Please enter a number.")
 }
+
+    }
+    }
+    
+    
