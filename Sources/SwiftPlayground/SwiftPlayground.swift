b02254c927f31b6c2187f126a89ee55ff1d7d8e4
emmettsillay-source <emmett.sillay@student.onslow.school.nz>
2026-05-18 22:04:22 +0000

    May 18, 2026, 10:04 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,5 +4,27 @@
 @main
 struct SwiftPlayground {
     static func main() {
+        var ingredients = [
+            ""
+        ]
+        var isRunning = true
+        while isRunning {
+            print("Enter your ingredients")
+            let input = readLine()
+        if input == "" {
+            isRunning = false
+        } else {
+            ingredients.append(input)
+        }
+
+        }
+    print("Ingredients list:")
+    for ingredient in ingredients {
+print(ingredients)
+if ingredient == "chocolatte" {
+    print("Yum!")
+}
+print("Lets start cooking!")
+    }
     }
 }
