2e7bee104977270e9c296379cfa23ebc7b8efeb0
emmettsillay-source <emmett.sillay@student.onslow.school.nz>
2026-06-16 21:43:33 +0000

    Jun 16, 2026, 9:43 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,255 +5,63 @@
 struct SwiftPlayground {
     static func main() {
 
-// Allows repetiton of the "birdRunning" while statement.
-var birdRunning = false
+/// Creates the constants for food types, food cost, drink types and drink costs. It also holds the variables for the total cost of the order and if the code is running. 
+let foods: [String] = [ "Burger", "Sushi", "Pizza", "Garlic bread", "Pork bun", "Dumplings" ]
+let foodsCost: [Double] = [7, 4, 5, 4, 5, 5]
+let drinks: [String] = [ "Juice", "Up & go", "Gatorade", "Water", "Coffee", "Fruit soda drink" ]
+let drinksCost: [Double] = [4, 4.5, 6, 3, 6, 4]
 
-// Allows the "Today you saw:" code for Birds to run.
-var endingCodeA = false
+let foodAndCost = Array(zip(foods, foodsCost))
+let drinksAndCost = Array(zip(drinks, drinksCost))
 
-// Allows the "Today you saw:" code for Insects to run.
-var endingCodeB = false
-
-// Allows repetition of the "insectRunning" while statement.
-var insectRunning = false
-
-// Array of bird species
-let birds: [String] = [
-    "Tieke",
-    "Kākā",
-    "Takahē",
-    "Hihi",
-    "Kiwi",
-    "Pāteke",
-    "Tūī",
-    "Kererū",
-    "Cancel"
-]
-
-// Use in birds Switch as the final tally counter for it.
-var birdsTally: [String] = [
-]
-
-// Unused currently.
-let insects: [String] = [
-    "Giant Wētā",
-    "Tree Wētā",
-    "Cave Wētā",
-    "Putoko",
-    "Pūngāwerewere",
-    "Ngaokeoke",
-    "Waemano",
-    "Kapowai",
-    "Cancel"
-]
-
-// Unused currently.
-var insectsTally: [String] = [
-]
-
-// Used for numbers before birds / insects.
-var counter = 0
-
-print("Welcome to the Animalfinder3000")
-print("Have you seen a Bird or Insect:")
+var totalCost: [Double] = [0]
+var isRunning = true
 
 
+
+// Prints welcome message / Asks user if they would like to order a drink / food.
+print("Welcome to SelfServe3001, Would you like to select a food or drink?")
+
+while isRunning {
 
 let input = readLine()!.lowercased()
 
-/*
+    if input == "food" {
+        print("You have selected: \(input)")
+        print()
 
+        print("Food Types:")
+        foodAndCost.enumerated().forEach { index, foodAndCost in
+        print("\(index + 1). \(foodAndCost.0) | $\(foodAndCost.1)")
+        }
+        print()
 
-BIRD SECTION
+        print("Enter a number:")
+        
+        if let input = readLine(), let index = Int(input), index >= 0 && index < foodsCost.count {
+            let priceAdding = foodsCost[index]
+            let currentTotal = totalCost.reduce(0, +)
+            let newTotal = currentTotal + priceAdding
+            totalCost = [newTotal]
+            print("Cost added to total. Current total = $\(totalCost)")
+        }
 
-
-*/
-// Prints first Question.
-if input == "bird" {
-    birdRunning = true
-    print()
-    print("What Bird have you seen (Type number):")
-
-// Prints out a number before the bird type.
-while birdRunning == true {
-    counter = 0
-    birds.forEach { bird in
-    counter = counter + 1
-    print("\(counter), \(bird)")
-    }
-
-// User input for bird seen.
-print("Enter a number:")
-print()
-let input = readLine()!
-let seenBird = Int(input)
-
-// Adds a tally to the tally variable for whichever bird user chose.
-switch seenBird {
-case 1: print("Tally added to \(birds[0]), would you like to add another: Y/N")
-birdsTally.append(birds[0])
-switcProgram1()
-
-case 2: print("Tally added to \(birds[1]), would you like to add another: Y/N")
-birdsTally.append(birds[1])
-switcProgram1()
-
-case 3: print("Tally added to \(birds[2]), would you like to add another: Y/N")
-birdsTally.append(birds[2])
-switcProgram1()
-
-case 4: print("Tally added to \(birds[3]), would you like to add another: Y/N")
-birdsTally.append(birds[3])
-switcProgram1()
-
-case 5: print("Tally added to \(birds[4]), would you like to add another: Y/N")
-birdsTally.append(birds[4])
-switcProgram1()
-
-case 6: print("Tally added to \(birds[5]), would you like to add another: Y/N")
-birdsTally.append(birds[5])
-switcProgram1()
-
-case 7: print("Tally added to \(birds[6]), would you like to add another: Y/N")
-birdsTally.append(birds[6])
-switcProgram1()
-
-case 8: print("Tally added to \(birds[7]), would you like to add another: Y/N")
-birdsTally.append(birds[7])
-switcProgram1()
-
-case 9: print("Canceled")
-birdRunning = false
-
-default: print("Input incorrect, try again.")
-}
-endingCodeA = true
-}
-
-// Adds a tally to the tally variable.
-let tally = birdsTally.reduce(into: [:]) { counts, birdsTally in
-    counts[birdsTally, default: 0] += 1
-}
-
-if endingCodeA {
-print()
-print("Today you saw:")
-}
-
-tally.forEach { birds, count in
-print("\(count). \(birds)")
-}
-
-/* 
-
-
-INSECT SECTION
-
-
-
-*/
-// First question.
-} else if input == "insect" {
-print()
-
-insectRunning = true
-
-// Prints out a number before the insect type.
-while insectRunning {
-    counter = 0
-    insects.forEach { insects in
-    counter = counter + 1
-    print("\(counter), \(insects)")
-    }
-
-// User input for insect seen.
-print("Enter a number:")
-print()
-let input = readLine()!
-let seenInsect = Int(input)
-
-// Adds a tally to the tally variable for whichever insect user chose.
-switch seenInsect {
-case 1: print("Tally added to \(insects[0]), would you like to add another: Y/N")
-insectsTally.append(insects[0])
-switcProgram2()
-
-case 2: print("Tally added to \(insects[1]), would you like to add another: Y/N")
-insectsTally.append(insects[1])
-switcProgram2()
-
-case 3: print("Tally added to \(insects[2]), would you like to add another: Y/N")
-insectsTally.append(insects[2])
-switcProgram2()
-
-case 4: print("Tally added to \(insects[3]), would you like to add another: Y/N")
-insectsTally.append(insects[3])
-switcProgram2()
-
-case 5: print("Tally added to \(insects[4]), would you like to add another: Y/N")
-insectsTally.append(insects[4])
-switcProgram2()
-
-case 6: print("Tally added to \(insects[5]), would you like to add another: Y/N")
-insectsTally.append(insects[5])
-switcProgram2()
-
-case 7: print("Tally added to \(insects[6]), would you like to add another: Y/N")
-insectsTally.append(insects[6])
-switcProgram2()
-
-case 8: print("Tally added to \(insects[7]), would you like to add another: Y/N")
-insectsTally.append(insects[7])
-switcProgram2()
-
-case 9: print("Canceled")
-insectRunning = false
-
-default: print("Input incorrect, try again.")
-}
-}
-endingCodeB = true
-} else {
-    print("Wrong input.")
-}
-
-// Prints what Birds / Insects user saw and how many.
-if endingCodeB {
-print()
-print("Today you saw:")
-}
-// Adds a tally to the tally variable.
-let tally = insectsTally.reduce(into: [:]) { counts, insectsTally in
-    counts[insectsTally, default: 0] += 1
-}
-tally.forEach { insects, count in
-print("\(count). \(insects)")
-}
-
-// Function for bird section switch
-// y = continues the loop, n = stops the loop, anything else is invalid
-func switcProgram1() {
-    let input = readLine()!.lowercased()
-if input == "y" {
-birdRunning = true
-} else if input == "n" {
-    birdRunning = false
-} else {
-    print("Invalid input.")
-}
-
-}
-
-func switcProgram2() {
-    let input = readLine()!.lowercased()
-    if input == "y" {
-        insectRunning = true
-    } else if input == "n" {
-        insectRunning = false 
+    } else if input == "drink" {
+        print("You have selected: \(input)")
+        print()
+        print("Food Types:")
+        drinks.enumerated().forEach { index, drinks in
+        print("\(index + 1). \(drinks)")}
+        print()
+        print("Enter a number:")
     } else {
-        print("Invalid input.")
+        print("Invalid input, try again.")
+    }
+
+}
+
+
     }
 }
 
-    }
-    }
+
